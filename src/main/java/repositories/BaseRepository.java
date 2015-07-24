b9a53ca731fedd9fb521f208e833c5d63d26ea26
priyank <priyankgandhi@gmail.com>
2015-07-23 21:51:52 -0700

    fixed jpa repository

--- src/main/java/repositories/BaseRepository.java
+++ src/main/java/repositories/BaseRepository.java
@@ -2,14 +2,14 @@ package repositories;
 
 import java.io.Serializable;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
 @NoRepositoryBean
-interface BaseRepository<T, ID extends Serializable> extends Repository<T, ID> {
+interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 
-  T findById(Integer id);
-
-  T save(T entity);
-  T saveAndFlush(T entity);
+	T findById(Integer id);
+	
 }
